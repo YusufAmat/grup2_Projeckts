10863d0fc748817600015acd106a14fede68a4a6
YusufAmat <hoccayusuf@gmail.com>
2022-10-25 09:56:31 +0200

    Logical Operators

---
+++ src/day12/IfStatement.java
@@ -0,0 +1,21 @@
+package day12;
+
+public class IfStatement {
+    public static void main(String[] args) {
+        //if statement kullanımı
+        if(false) {
+            System.out.println("Merhaba");
+        } else if (false) {
+            System.out.println("Hi");
+        }else {
+            System.out.println("Hallo!");
+        }
+
+
+        if(true){
+            System.out.println("Salute");
+        }else if(true){
+            System.out.println("Selam");
+        }
+    }
+}
