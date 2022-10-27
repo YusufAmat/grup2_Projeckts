59925bc38f97bb03cf775c246cbdd6d8570a6dbf
YusufAmat <hoccayusuf@gmail.com>
2022-10-27 11:06:29 +0200

    Control Flow Statements for

---
+++ src/day16/Loops6.java
@@ -0,0 +1,14 @@
+package day16;
+
+public class Loops6 {
+    public static void main(String[] args) {
+        int toplam=0;
+        double ort=0.0;
+        for (int i = 0; i <=10 ;i++) {
+            toplam=toplam+i;
+        }
+        ort= toplam/10.0;
+        System.out.println("Toplam = " + toplam);
+        System.out.println("Ortalama = "+ort);
+    }
+}
