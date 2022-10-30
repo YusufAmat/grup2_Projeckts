b818ade6d73d263f933eccc595d1eea37f86560a
YusufAmat <hoccayusuf@gmail.com>
2022-10-30 18:36:04 +0100

    Control Flow Statements for 2

--- src/day17/ForTask5.java
+++ src/day17/ForTask5.java
@@ -1,6 +1,6 @@
 package day17;
 
-public class ForLoop5 {
+public class ForTask5 {
     public static void main(String[] args) {
         int enterSayaci=0;
         for (int i = 5; i <140 ; i+=2) {
