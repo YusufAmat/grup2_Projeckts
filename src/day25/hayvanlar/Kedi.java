cc9a82b3357c48bea72b9226326627016ebeb44a
YusufAmat <hoccayusuf@gmail.com>
2022-11-17 14:00:55 +0100

    OOP 2

---
+++ src/day25/hayvanlar/Kedi.java
@@ -0,0 +1,15 @@
+package day25.hayvanlar;
+
+public class Kedi {
+    public String cinsi;
+    public int yasi;
+    public double agirligi;
+
+
+    public Kedi(String c,int y, double a){
+        cinsi=c;
+        yasi=y;
+        agirligi=a;
+    }
+
+}
