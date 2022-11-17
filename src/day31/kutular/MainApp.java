c28225aa9b6136f5fff3beb1677b7f8f4663bab5
YusufAmat <hoccayusuf@gmail.com>
2022-11-17 22:38:27 +0100

    OOP - Encapsulation

---
+++ src/day31/kutular/MainApp.java
@@ -0,0 +1,21 @@
+package day31.kutular;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Kutu kutu1 = new Kutu();
+        kutu1.setGenislik(5);
+        int hacim = kutu1.getHacim();
+        System.out.println(kutu1);
+        System.out.println("Hacim = " + hacim);
+
+        Kutu kutu2 = new Kutu(-5,3,4);
+        System.out.println(kutu2);
+        System.out.println("Hacim = " + kutu2.getHacim());
+
+        RenkliKutu rk1 = new RenkliKutu();
+        rk1.setGenislik(10);
+        rk1.setRenk("Kirmizi");
+        System.out.println(rk1);
+        System.out.println("Renkli kutunun hacmi = " + rk1.getHacim());
+    }
+}
