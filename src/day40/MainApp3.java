8d645a502b9b82f4b5fefb4760adba45226afc02
YusufAmat <hoccayusuf@gmail.com>
2022-11-30 21:34:06 +0100

    Strings

--- src/day40/MainApp3.java
+++ src/day40/MainApp3.java
@@ -2,8 +2,8 @@ package day40;
 
 public class MainApp3 {
     public static void main(String[] args) {
-        //empty
-        // blank
+        //empty--> bos mu degil mi? bossa true, degilse false.
+        // blank--> Icinde herhangi bi karakter var mi yok mu ona bakar. Varsa false, yoksa true.
 
         String str ="";
         System.out.println(str.isEmpty());
